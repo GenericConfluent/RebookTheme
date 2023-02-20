51de2a83aa24ec0c5c45df388a4abfad911fe1d3
GenericConfluent <m13power@gmail.com>
2023-02-20 14:25:56 -0700

    Initial

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+
+let pallet = ["606c38", "283618", "fefae0", "dda15e", "bc6c25"];
+
+module.exports = {
+  content: [
+    "./layout/**/*.{html,liquid}",
+    "./sections/**/*.{html,liquid}"
+  ],
+  theme: {},
+  plugins: [],
+}
